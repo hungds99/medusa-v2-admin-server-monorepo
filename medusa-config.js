d3f123474dda96a6f9226cb7ed896e96052c0d1b
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-07-06 17:07:13 +0700

    add admin configuration

--- medusa-config.js
+++ medusa-config.js
@@ -1,6 +1,6 @@
-import { loadEnv, defineConfig } from '@medusajs/utils'
+import { loadEnv, defineConfig } from '@medusajs/utils';
 
-loadEnv(process.env.NODE_ENV, process.cwd())
+loadEnv(process.env.NODE_ENV, process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -9,8 +9,12 @@ module.exports = defineConfig({
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
       authCors: process.env.AUTH_CORS,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+  },
+  admin: {
+    disable: false,
+  },
+  plugins: [],
+});
